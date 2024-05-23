c22358f7e31400ebe970db3844ededd1bc376c64
cogginsmark <cogginsmark@icloud.com>
2024-05-22 18:51:08 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@
     </card-container>
     <foot v-bind:author="author" v-bind:footer="footer" />
     <light-toggle v-on:click="toggleTheme()"><span v-if="!isDark" >💡</span><span v-if="isDark">💡</span></light-toggle>
-    <div>
+    <div id="app">
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
     <router-link to="/contact">Contact</router-link>
