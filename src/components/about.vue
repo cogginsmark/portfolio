3f2e4f7f36aa09f3b0dd1e86e5bb2edfffdc3e84
cogginsmark <cogginsmark@icloud.com>
2024-05-22 18:12:29 -0700

    Update about.vue

--- src/components/about.vue
+++ src/components/about.vue
@@ -2,19 +2,58 @@
   <theme-provider :theme="theme">
     <hero
       v-bind:author="author"
-      v-bind:subtitle="hero.subtitle"
+      v-bini d:subtitle="hero.subtitle"
       v-bind:description="hero.description"
+      v-bind:cta="hero.cta"
     />
+    <card-container id="products">
+      <card
+        v-for="(product, index) in products.featured" :key="index"
+        v-bind:name="product.name"
+        v-bind:medium="product.medium"
+        v-bind:summary="product.summary"
+        v-bind:copy="product.copy"
+        v-bind:links="product.links"
+        v-bind:images="product.images"
+      />
+      <minor-card-container>
+        <minor-card
+          v-for="(product, index) in products.minor && products.minor.slice(0,3)" :key="index"
+          v-bind:name="product.name"
+          v-bind:description="product.description"
+          v-bind:link="product.link"
+        />
+      </minor-card-container>
+      <logo-section-container>
+        <main-title>{{logoSection.title}}</main-title>
+        <logo-container>
+          <logo-card
+            v-for="(logo, index) in logoSection.logos && logoSection.logos.slice(0,3)" :key="index"
+            v-bind:src="logo.src"
+            v-bind:url="logo.url"
+            v-bind:alt="logo.alt"
+          />
+        </logo-container>
+      </logo-section-container>
+    </card-container>
     <foot v-bind:author="author" v-bind:footer="footer" />
     <light-toggle v-on:click="toggleTheme()"><span v-if="!isDark" >💡</span><span v-if="isDark">💡</span></light-toggle>
-  </div>
   </theme-provider>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
+    <router-view/>
+  </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import styled from 'vue-styled-components'
 import Hero from './components/Hero.vue'
+import Card from './components/Card.vue'
+import MinorCard from './components/MinorCard.vue'
+import LogoCard from './components/LogoCard.vue'
 import Foot from './components/Foot.vue'
 import { ThemeProvider, injectGlobal } from 'vue-styled-components'
 
