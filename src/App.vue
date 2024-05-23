0000cc7cb10af18e2294df67fc472957ac13f7e5
cogginsmark <cogginsmark@icloud.com>
2024-05-22 20:41:15 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/contact">Contact</router-link>
+      <router-link class="nav-link" to="/">Home</router-link>
+      <router-link class="nav-link" to="/about">About</router-link>
+      <router-link class="nav-link" to="/contact">Contact</router-link>
     </nav>
     <router-view/>
   </div>
