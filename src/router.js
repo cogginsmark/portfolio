4a9df43e5d3f56cba56da206f3a9a164e78ed87a
cogginsmark <cogginsmark@icloud.com>
2024-05-22 17:28:43 -0700

    fixed

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import About from './components/About.vue'
-import Contact from './components/Contact.vue'
+import About from './components/about.vue'
+import Contact from './components/contact.vue'
 
 Vue.use(Router)
 
