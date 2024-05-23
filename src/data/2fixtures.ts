52712a3964a16a94509389c5fad0e42d3b20d907
cogginsmark <cogginsmark@icloud.com>
2024-05-22 20:09:45 -0700

    Update 2fixtures.ts

--- src/data/2fixtures.ts
+++ src/data/2fixtures.ts
@@ -2,7 +2,11 @@ export default {
   author: "Coggins",
   hero: {
     subtitle: "Graphic Designer",
-    description: "Residence - Turlock, CA\nEducation - California State University Stanislaus\nMajor - Bachelor of Communication Studies, Bachelor of Creative Media\nInternships - Bay Valley Tech (6 Months) Position: Lead Design Intern\nExperience - Graphic Design Student Assistant position at Associated Students Inc, at California State University Stanislaus"
+    description: `Residence - Turlock, CA
+    Education - California State University Stanislaus
+    Major - Bachelor of Communication Studies, Bachelor of Creative Media
+    Internships - Bay Valley Tech (6 Months) Position: Lead Design Intern
+    Experience - Graphic Design Student Assistant position at Associated Students Inc, at California State University Stanislaus`,
   },
   footer: {
     tagline: "Mark Coggins — Turlock, California",
